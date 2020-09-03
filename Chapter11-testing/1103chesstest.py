b1d39e04d5f29a75d793a26db9cce61ff1b55588
Xu <jiaquan.xu@intel.com>
2020-09-02 22:17:15 -0700

    update

---
+++ Chapter11-testing/1103chesstest.py
@@ -0,0 +1,11 @@
+
+# random crashes:
+# 1. Random variable
+# 2. Uninitialized variable (could use runtime tools to check)
+# 3. Memory leak
+# 4. External dependencies
+# To tracking the issue, learning as much as possible about the application, check components or scenarios. Try to test every case and eliminate several possibilities. 
+    
+    
+
+        
