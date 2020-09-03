8cb289f3a0fa35776179b1e02bd18662e6459114
Xu <jiaquan.xu@intel.com>
2020-09-02 22:33:09 -0700

    update

---
+++ Chapter11-testing/1106testanatm.py
@@ -0,0 +1,8 @@
+
+# To test a pen, ask "who, what, where, when, how, why" of a problem. 
+# consider what the different components of the object or problem
+# test both intended and unintended scenarios
+    
+    
+
+        
