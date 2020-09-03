67df4921c2e995adcaea098a049e27d0ee12ec8d
Xu <jiaquan.xu@intel.com>
2020-09-02 22:23:50 -0700

    update

---
+++ Chapter11-testing/1104notesttools.py
@@ -0,0 +1,14 @@
+
+# to perform load testing, we must first identify the performance critical scenarios and the metrics which fulfill our performance objectives. Thypical criteria include:
+# 1. response time
+# 2. throughput
+# 3. resource utilization
+# 4. maximum load that the system can bear
+# In the absense of formal testing tools, we could create our own. For example, we could simulate concurrent users by creating thousands of vitual users. We would 
+# write a multi-threaded program with thousands of threads, where each thread acts as a real-world user loading the page. For each user, we would 
+# programmatically measure response time, data I/O, etc.
+# We would then analyze the results based on the data gathered during the tests and compare it with the accepted values. 
+    
+    
+
+        
